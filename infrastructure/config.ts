cf763ca9ddae2ff8da730a96a3b74d521fd5c186
Felipe Trindade <felipelaptrin@gmail.com>
2024-01-31 11:05:05 -0300

    add github oidc

--- infrastructure/config.ts
+++ infrastructure/config.ts
@@ -1,5 +1,13 @@
 import { IFrontend } from './frontend'
+import { IGitHubOidc } from './oidc'
+
 
 export const frontendConfig: IFrontend = {
   domainName: "felipetrindade.com",
+}
+
+export const githubOidcConfig: IGitHubOidc = {
+  roleName: "GithubActionsOidcWebsite",
+  organization: "felipelaptrin",
+  repository: "felipetrindade.com",
 }
