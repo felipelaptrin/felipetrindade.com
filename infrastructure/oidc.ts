fa4a1eda153683e39066b52333a913369ffdeace
Felipe Trindade <52676348+felipelaptrin@users.noreply.github.com>
2024-01-31 17:43:54 -0300

    add infra ci/cd (#2)
    * add ci
    
    * use a single file
    
    * add missing identity provider for GitHub
    
    * add working directory
    
    * wip
    
    * wip
    
    * test stack name
    
    * change password
    
    * fix workflow
    
    * add env var
    
    * add node to ci
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: Felipe Trindade <felipelaptrin@gmail.com>

--- infrastructure/oidc.ts
+++ infrastructure/oidc.ts
@@ -1,5 +1,6 @@
 import { iam } from "@pulumi/aws"
 import { accountId } from "./commons"
+import { interpolate } from "@pulumi/pulumi"
 
 export interface IGitHubOidc {
   roleName: string
@@ -15,34 +16,45 @@ export class GitHubOidc {
   constructor(id: string, props: IGitHubOidc) {
     this.id = id
     this.props = props
+    this.setIdentityProvider()
     this.role = this.getOidcRole()
   }
 
+  setIdentityProvider(): void {
+    new iam.OpenIdConnectProvider(`${this.id}-identity-provider`, {
+      url: "https://token.actions.githubusercontent.com",
+      clientIdLists: ["sts.amazonaws.com"],
+      thumbprintLists: [
+        "1b511abead59c6ce207077c0bf0e0043b1382612"
+      ]
+    })
+  }
+
   getOidcRole(): iam.Role {
-    const trustRelationship = {
-      Version: "2012-10-17",
-      Statement: [{
-        Effect: "Allow",
-        Principal: {
-          Federated: `arn:aws:iam::${accountId}:oidc-provider/token.actions.githubusercontent.com`
+    const trustRelationship = interpolate`{
+      "Version": "2012-10-17",
+      "Statement": [{
+        "Effect": "Allow",
+        "Principal": {
+          "Federated": "arn:aws:iam::${accountId}:oidc-provider/token.actions.githubusercontent.com"
         },
-        Action: "sts:AssumeRoleWithWebIdentity",
-        Condition: {
-          StringEquals: {
-            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+        "Action": "sts:AssumeRoleWithWebIdentity",
+        "Condition": {
+          "StringEquals": {
+            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
           },
-          StringLike: {
-            "token.actions.githubusercontent.com:sub": `repo:${this.props.organization}/${this.props.repository}:*`
+          "StringLike": {
+            "token.actions.githubusercontent.com:sub": "repo:${this.props.organization}/${this.props.repository}:*"
           }
         }
       }]
-    }
+    }`
 
     const role = new iam.Role(`${this.id}-role`, {
       name: this.props.roleName,
       description: "Role assumed by the GitHub Actions to deploy resources related to the website/blog",
-      assumeRolePolicy: JSON.stringify(trustRelationship),
-      managedPolicyArns: ["arn:aws:iam::aws:policy/AdministratorAccess"]
+      assumeRolePolicy: trustRelationship,
+      managedPolicyArns: ["arn:aws:iam::aws:policy/AdministratorAccess"],
     })
 
     return role
