8c13b580e9beabce9fa4bfcd83e776d082eaaae2
Felipe Trindade <felipelaptrin@gmail.com>
2024-02-19 17:29:50 -0300

    fix typo

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -70,7 +70,7 @@ export default BlogIndex
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo />
+export const Head = () => <Seo title="Felipe Trindade" />
 
 export const pageQuery = graphql`
   {
